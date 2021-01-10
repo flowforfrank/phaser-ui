839fbd2f50d4fa57026be40df3c2642b508550ef
Ferenc Almasi <v-falmasi@hotels.com>
2021-01-10 20:59:31 +0100

    🎨 Simplify code of disabled buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -44,20 +44,18 @@ class Button {
         this.scene = scene;
         this.layout = layout;
 
-        if (this.disabled) {
-            this.container = scene.add.text(x, y, this.label, this.styles.disabled)
-                .setOrigin(0)
-                .setPadding(styles.padding);
-        } else {
-            this.container = scene.add.text(x, y, this.label, this.styles)
-                .setOrigin(0)
-                .setPadding(styles.padding)
-                .setInteractive({ useHandCursor: true })
-                .on('pointerdown', () => this.callback(`#${this.label}${x}${y}`))
-                .on('pointerover', () => this.container.setStyle({ fill: this.styles.hoverBackgroundColor }))
-                .on('pointerout', () => this.container.setStyle({ fill: this.styles.color }));
-        }
+        this.container = scene.add.text(x, y, this.label, this.styles)
+            .setOrigin(0)
+            .setPadding(styles.padding)
+            .setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => this.callback(`#${this.label}${x}${y}`))
+            .on('pointerover', () => this.container.setStyle({ fill: this.styles.hoverBackgroundColor }))
+            .on('pointerout', () => this.container.setStyle({ fill: this.styles.color }));
 
+        if (this.disabled) {
+            this.container.disableInteractive()
+                .setStyle(this.styles.disabled);
+        }
 
         if (layout) {
             layout.add(this.container);
