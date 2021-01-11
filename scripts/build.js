0f409af82e9cce2598ff5bdad90eb48bdd591d7b
Ferenc Almasi <v-falmasi@hotels.com>
2021-01-11 11:02:54 +0100

    🚀 Add build script

---
+++ scripts/build.js
@@ -0,0 +1,27 @@
+const Bundler = require('parcel-bundler');
+const fs = require('fs');
+
+const componentsPath = './src/components';
+const indexFilePath = './dist/index.js';
+const indexExports = [];
+const options = {
+    outDir: './dist',
+    sourceMaps: false,
+    autoInstall: false,
+    minify: true,
+    watch: false
+};
+
+const components = fs.readdirSync(componentsPath);
+
+(async () => {
+    for (const component of components) {
+        const entry = `${componentsPath}/${component}`;
+
+        indexExports.push(`export { default as ${component.split('.')[0]} } from './${component}'`);
+
+        await new Bundler(entry, options).bundle();
+    }
+
+    fs.writeFileSync(indexFilePath, indexExports.join('\n'));
+})();
